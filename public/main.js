64487040f2c9004cbd75de7b967d787ffa5953b9
Jessica Wang <jessikka@email.unc.edu>
2022-04-17 15:01:33 -0400

    flip a coin button working

--- public/main.js
+++ public/main.js
@@ -1,6 +1,22 @@
 // Focus div based on nav button click
 
 // Flip one coin and show coin image to match result when button clicked
+const single = document.getElementById('singlenav')
+
+single.addEventListener('click', flipCoin)
+
+async function flipCoin(event) {
+    const url = 'http://localhost:5000/app/flip/'
+    fetch(url, {mode: 'cors'})
+    .then(function(response) {
+        return response.json()
+    })
+    .then(function(result) {
+        console.log(result)
+        document.getElementById('result').innerHTML = result.flip
+        document.getElementById('single').setAttribute('class', 'active')
+    })
+}
 
 // Flip multiple coins and show coin images in table as well as summary results
 // Enter number and press button to activate coin flip series
